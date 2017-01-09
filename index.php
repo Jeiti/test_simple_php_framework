e6758da7183c111cca2cf6b267f95006cd572f92
Jeiti <jeiti@list.ru>
2017-01-09 14:56:32 +0300

    Изменен обратно index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 header("Content-type:text/html; charset=utf-8");
 
-//use framework\FrontController;
-//use Pimple\Container;
+use framework\FrontController;
+use Pimple\Container;
 
-require('vendor/pimple/pimple/src/Pimple/Container.php');
-require('framework/FrontController.php');
+require_once('framework/FrontController.php');
 require('vendor/autoload.php');
 $container = new Container();
 require('config/params.php');
